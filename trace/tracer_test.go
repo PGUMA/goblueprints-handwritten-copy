95a3f37ce8d999acf10702366e4eb9cd4bcf38d2
pguma <p.guma.service+git@gmail.com>
2023-12-15 17:41:32 +0900

    add trace logic and test

---
+++ trace/tracer_test.go
@@ -0,0 +1,19 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("return value is nil.")
+	} else {
+		tracer.Trace("Hello Golang")
+		if buf.String() != "Hello Golang\n" {
+			t.Errorf("'%s' is incorrect message", buf.String())
+		}
+	}
+}
